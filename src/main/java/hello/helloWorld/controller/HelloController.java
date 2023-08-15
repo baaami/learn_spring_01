3e9c9c216a942cbc7830785d8b22564160fca1aa
Seongbeom27 <lgh121546@naver.com>
2023-08-15 14:53:09 +0900

    API JSON 응답 방식 학습

--- src/main/java/hello/helloWorld/controller/HelloController.java
+++ src/main/java/hello/helloWorld/controller/HelloController.java
@@ -3,20 +3,52 @@ package hello.helloWorld.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
     // hello 경로로 들어오면 해당 클래스 수행
+    // 파일을 그대로 내려줌
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
     }
 
+    // 파일을 그대로 내려주되 동적으로 값을 할당하여 내려줌
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        // return되는 string을 그대로 html 화면에 뿌려줌
+        return "hello " + name; // "hello spring"
+    }
+
+    // Json 데이터를 응답으로 반환함
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
